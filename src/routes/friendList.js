df1db0cf1903060da2c711e251cfff9d8931aadf
raffrearaUS <raffrearaus@gmail.com>
2019-12-18 09:04:35 +0100

    Added remaining friends requests

--- src/routes/friendList.js
+++ src/routes/friendList.js
@@ -1,11 +1,22 @@
 const FriendListService = require("../services/friendListService.js");
 let express = require('express');
 let router = express.Router();
-let models = require('../models/friendlist.model');
 
 router.get('/users/:id/friends', (req, res) => {
     FriendListService.getFriends(req.params.id).then(data => {
         res.send(data);
+    });
+});
+
+router.delete('/users/:id/friends', (req, res) => {
+    FriendListService.removeFriends(req.params.id).then(data => {
+        res.sendStatus(data);
+    });
+});
+
+router.delete('/users/:id/friends/:friendId', (req, res) => {
+    FriendListService.removeFriend(req.params.id, req.params.friendId).then(data => {
+        res.sendStatus(data);
     });
 });
 
--- src/services/friendListService.js
+++ src/services/friendListService.js
@@ -12,6 +12,30 @@ class FriendListService {
             });
         });
     }
+
+    static removeFriends(userId) {
+        return new Promise((resolve, reject) => {
+            models.FriendList.remove({userId: userId}, (err) => {
+                resolve(204);
+            });
+        });
+    }
+
+    static removeFriend(userId, friendId) {
+        return new Promise((resolve, reject) => {
+            models.FriendList.findOne({userId: userId}, (err, friends) => {
+                if (!friends || !friends.friends.includes(friendId)) resolve(404);
+                else  {
+                    var friendIndex = friends.friends.indexOf(friendId);
+                    var friendsCopy = friends.friends;
+                    friendsCopy.splice(friendIndex, 1);
+                    models.FriendList.updateOne({userId: userId}, {friends: friendsCopy}, (err) => {
+                        resolve(204);
+                    });
+                }
+            });
+        });
+    }
 }
 
 module.exports = FriendListService;
