61ae652a1ac2013c74f91a351caa39e0baeaee5d
raffrearaUS <raffrearaus@gmail.com>
2019-12-31 11:46:37 +0100

    Added Dockerfile

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ app.use((err, req, res, next) => {
     res.sendFile(path.join(__dirname, '../public/500.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => console.info(`Server has started on port ${PORT}`));
 
